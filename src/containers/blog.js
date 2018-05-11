7d0a6f530ecbc3e1201d4c09372a609ceede8d7f
jim-at-jibba <jim@justjibba.net>
2018-05-11 07:36:13 +0100

    Added base templates

---
+++ src/containers/blog.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// RR Switch will be here to switch betweeen single and list views
+const Blog = () => (
+  <div>
+    <p>Blog</p>
+  </div>
+);
+
+export default Blog;
--- src/containers/main.js
+++ src/containers/main.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Home from "./home.js";
+
+import Home from "./home";
+import About from "./about";
+import Blog from "./blog";
+import Images from "./images";
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/blog" component={Blog} />
+      <Route path="/images" component={Images} />
     </Switch>
   </main>
 );
